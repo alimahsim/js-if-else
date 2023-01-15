602f7ef730cde8cac17e502bcf5da9b5e355e341
alimahsim <alimahsim@gmail.com>
2023-01-15 22:32:56 +0500

    learning if and else in JS

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+//console.log("Hello World!");
+/*
+
+// BMI = body mass index
+
+let height = 1.75;
+let weight = 67;
+let BMI = weight / (height*height);
+
+console.log(BMI);
+*/
+
+// Counting voting age to participate
+
+/*
+let personAge = 17;
+let votingAge = 18;
+
+if (personAge >= votingAge){
+    console.log("You can vote")
+}else{
+    console.log("Unfortunatly, you can't vote.")
+}
+*/
+
+// Student's marking system
+
+let studentMark = 55;
+
+if (studentMark >= 90){
+    console.log("You have got A , excellent!");
+}else if (studentMark >= 80 && studentMark < 90){
+    console.log("You have got B");
+}else if (studentMark >=70 && studentMark < 80){
+    console.log("You have got C");
+}else if (studentMark >=60 && studentMark > 70){
+    console.log("You have got D");
+}else{
+    console.log("You have got under 60 ball and faild");
+}
+
+
